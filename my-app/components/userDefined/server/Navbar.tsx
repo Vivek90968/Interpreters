61590d285286373ebec4d302f6ea7e15896a2a19
gyan-cell <jha07944@gmail.com>
2024-03-16 20:18:17 +0530

    navbar done

--- my-app/components/userDefined/server/Navbar.tsx
+++ my-app/components/userDefined/server/Navbar.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+'use client'
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+import logo  from "../../../public/logo.png"
+import Link from 'next/link'
+import "@/styles/navbar.scss"
+import { FaBars, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [navActive , setNavActive]  = useState(false);
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='logo' >
+        <Image src={logo} alt='Project Onion'  />
+      </div>
+      <div className='mobSwitch text-4xl' onClick={()=>{setNavActive(!navActive)}}  >
+        {
+          navActive ?<FaXmark /> : <FaBars />
+        }
+      </div>
+      <div className={navActive ? "navOptions navComes" : "navOptions"} >
+        <ul>
+          <li><Link href={'login'} >Login</Link></li>
+          <li><Link href={'register'} >Register</Link></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
